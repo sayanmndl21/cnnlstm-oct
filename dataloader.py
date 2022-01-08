6602cc79daffafee267b9ed080b112c15ba4bf43
sayanmndl21 <sayanmndl21@github.com>
2022-01-08 13:07:33 +0530

    added univ dataloader, shallow network

--- dataloader.py
+++ dataloader.py
@@ -70,3 +70,72 @@ class CNNLSTMDataLoader(Dataset):
         return x,y,s
 
 
+class CLUnivDataLoader(Dataset):
+    def __init__(self, csv_dataset, img_folder, targetcol = 'slope',dropc = None , \
+    transform = transforms.Compose([transforms.Resize(128),transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]), \
+    timestep = 5, n=3):
+        self.dataset = pd.read_csv(csv_dataset)
+        if dropc:
+            self.dataset = self.dataset.drop(columns=dropc)
+        self.timestep = timestep
+        self.transform = transform
+        self.img_folder = img_folder
+        self.n = n
+        #self.dataset = self.dataset.iloc[::-1] #reverse
+        self.chunks, self.targets, self.seqlen = self.dataprep(targetcol)
+        
+    def dataprep(self, targetcol):
+        grouped = self.dataset.groupby('maskedeye')
+        #grouped = iter(grouped)
+        #_, chunk = next(grouped)
+        chunks = []
+        targets = []
+        seqlen = []
+        for _, group in grouped:
+            group = group.sort_values(by=['pdate'],ascending=True)
+            if len(group) == self.n:
+                chunks += [group['filename'].iloc[:self.n].to_list()]
+                targets += [group[targetcol].iloc[self.n-1]]
+                seqlen += [self.n]
+            elif len(group)>self.n:
+                #chunks += [group['filename'].iloc[-self.timestep:].to_list()]
+                #targets += [group[targetcol].iloc[-1]]
+                #seqlen += [self.timestep]
+                for i in range(self.n,group.shape[0]+1):
+                    if i < self.timestep:
+                        chunks += [group['filename'].iloc[:i].to_list()]
+                        targets += [group[targetcol].iloc[i-1]]
+                        seqlen += [i]
+                    else:
+                        chunks += [group['filename'].iloc[i-self.timestep:i].to_list()]
+                        targets += [group[targetcol].iloc[i-1]]
+                        seqlen += [i]
+        return chunks, targets, seqlen
+
+    def gettargets(self):
+        return self.targets
+
+    def __len__(self):
+        return len(self.chunks)
+    
+    def __getitem__(self, index):
+        files = self.chunks[index]
+        len_files = len(files)
+        if len_files < self.timestep:
+            files = ['empty']*(self.timestep - len_files) + files
+        images = []
+        for file in files:
+            if file == 'empty':
+                image = torch.zeros(3,64,64)
+            else:
+                path = os.path.join(self.img_folder,file)
+                image = PIL.Image.open(path)
+                if self.transform is not None:
+                    image = self.transform(image)
+            images += [image]
+        x = torch.stack(images,0)
+        y = self.targets[index]
+        s = self.seqlen[index]
+        
+        return x,y,s
+
---
+++ model.py
@@ -0,0 +1,111 @@
+import torch
+from torch.utils.data.dataset import Dataset
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.utils
+import torch.distributions
+
+n_encode = 512
+
+#timedistributed cnn network
+class ConvBlock(nn.Module):
+    def __init__(self, in_f, out_f, *args, **kwargs):        
+        super(ConvBlock, self).__init__()
+        self.seq = nn.Sequential(
+            nn.Conv2d(in_f, out_f, *args, **kwargs),
+            nn.BatchNorm2d(out_f),
+            nn.ReLU()
+            )
+
+    def forward(self, x):
+        return self.seq(x)
+
+class CNNEncoder(nn.Module):
+    def __init__(self, in_c, in_shape = 64 ,channels = [16, 32, 64], n_encode = 512):        
+        super(CNNEncoder, self).__init__()
+        self.channels = [in_c] + channels
+        conv_blocks = [ConvBlock(in_f, out_f, kernel_size=3,stride = 2 , padding=1) 
+                       for in_f, out_f in zip(self.channels, self.channels[1:])]
+        self.cnnmodule = nn.Sequential(*conv_blocks)
+        in_n = int(self.channels[-1]*(in_shape//(2**len(channels)))*(in_shape//(2**len(channels))))
+        self.linear = nn.Sequential(
+            nn.Dropout(0.3),
+            nn.Linear(in_n, n_encode), #change to in_n
+            nn.ReLU(),
+        )
+        
+
+    def forward(self, x):
+        x = self.cnnmodule(x)
+        print('xshape',x.shape)
+        x = x.view(x.size(0),-1)
+        print('xshape',x.shape)
+        x = self.linear(x)
+        return x
+
+    
+class TimeDistributed(nn.Module):
+    def __init__(self, layer, time_steps, *args, device = 'cpu'):        
+        super(TimeDistributed, self).__init__()
+        self.device = device
+        self.layers = nn.ModuleList([layer(*args) for i in range(time_steps)])
+
+    def forward(self, x):
+
+        batch_size, time_steps, C, H, W = x.size()
+        output = torch.tensor([]).to(self.device)
+        for i in range(time_steps):
+          output_t = self.layers[i](x[:, i, :, :, :])
+          output_t  = output_t.unsqueeze(1)
+          output = torch.cat((output, output_t ), 1)
+        print('out',output.shape)
+        return output
+
+class LSTMModule(torch.nn.Module):
+    def __init__(self,n_features, n_hidden, n_layers, seq_len, n_out, device = 'cpu', bidirectional = True):
+        super(LSTMModule, self).__init__()
+        self.n_features = n_features
+        self.n_hidden = n_hidden # number of hidden states
+        self.n_layers = n_layers # number of LSTM layers (stacked)
+        self.device = device
+    
+        self.l_lstm = nn.LSTM(input_size = self.n_features, 
+                                 hidden_size = self.n_hidden,
+                                 num_layers = self.n_layers, 
+                                 batch_first = True,
+                                 bidirectional = bidirectional,
+                                 dropout = 0.3)
+        # according to pytorch docs LSTM output is 
+        # (batch_size,seq_len, num_directions * hidden_size)
+        l = 2 if bidirectional else 1
+        self.decoder_network = nn.Sequential(
+            nn.Linear(self.n_hidden*seq_len*l, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_out)
+        )
+        
+    
+    def forward(self, data, device = 'cpu'):        
+        batch_size, seq_len, _ = data.size()
+        hidden_state0 = torch.zeros(self.n_layers*2,batch_size,self.n_hidden).to(self.device)
+        cell_state0 = torch.zeros(self.n_layers*2,batch_size,self.n_hidden).to(self.device)
+
+        lstm_out, (hidden,cell) = self.l_lstm(data.float(),(hidden_state0,cell_state0))
+             
+        out = lstm_out.contiguous().view(batch_size,-1)
+        print('lstm', out.shape)
+        out = self.decoder_network(out)
+        return out
+
+
+class CNNLSTMNet(nn.Module):
+    def __init__(self, channels = 3, ts = 5, n_out = 1, device = 'cpu'):
+        super(CNNLSTMNet, self).__init__()
+        self.ts_cnn = TimeDistributed(CNNEncoder, ts, channels,device = device)
+        self.lstm_decoder = LSTMModule(n_encode, 64, 2, ts, n_out, device=device)
+
+    def forward(self, x):
+        x = self.ts_cnn(x)
+        x = self.lstm_decoder(x)
+        return x
+
